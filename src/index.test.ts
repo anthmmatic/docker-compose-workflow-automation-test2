c8883e278af38055b16a7be84aecb201567395d2
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-03-07 22:55:13 +0100

    chore: test with native fetch

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import { expect, it } from 'vitest';
 
-it('should ping server', async () => {
+it('should ping server (fetch)', async () => {
+  const response = await fetch('http://localhost:3000');
+
+  const txt = await response.text();
+
+  expect(txt).toBe('Hello Hono!');
+});
+
+it('should ping server (axios)', async () => {
   const response = await axios.get('http://localhost:3000');
 
   const txt = response.data;
